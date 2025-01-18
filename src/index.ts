ae5471f4a4cc7895c62a31b4a45527de662d5f98
David <goyaldavid55@gmail.com>
2025-01-18 23:02:51 +0530

    added slot

--- src/index.ts
+++ src/index.ts
@@ -30,13 +30,42 @@ app.get("/txn", async (req: Request, res: Response) => {
 			res.status(404).json({ success: false, error: "Token not found" });
 			return;
 		}
+
+		const data = {
+			method: "getTransaction",
+			jsonrpc: "2.0",
+			id: "1",
+			params: [
+				txn.transaction,
+				{
+					encoding: "jsonParsed",
+					commitment: "confirmed",
+					maxSupportedTransactionVersion: 0,
+				},
+			],
+		};
+
+		const response = await fetch(
+			"https://mainnet.helius-rpc.com/?api-key=e136916d-87a5-4dfe-87ab-0ba99d068979",
+			{
+				method: "POST",
+				body: JSON.stringify(data),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+
+		const json = await response.json();
 		res.status(200).json({
 			success: true,
 			txn: txn.transaction,
 			creator: txn.creator,
 			initialMint: txn.initialMint,
 			createdAt: txn.createdAt,
+			slot: json.result.slot,
 		});
+		return;
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({ success: false, error: "Internal server error" });
